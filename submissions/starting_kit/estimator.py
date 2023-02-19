b50b2ba6a20aeba370c3d93562a09ea19de4a449
Windu12345 <32morvantheo@gmail.com>
2023-02-19 19:29:30 +0100

    adding the estimator file to the repo

---
+++ submissions/starting_kit/estimator.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestClassifier
+
